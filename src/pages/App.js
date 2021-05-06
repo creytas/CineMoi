ea350bb0a75e7bc935852ab716e6fd37c43e601d
CR3Y745 <creytas@gmail.com>
2021-05-06 13:06:25 +0100

    feat : update of the component's style

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Home from "../components/Home";
+import Footer from "../components/Footer";
 import "../styles/index.css";
 function App() {
   return (
     <div>
       <Header />
       <Home />
+      <Footer/>
     </div>
   );
 }
